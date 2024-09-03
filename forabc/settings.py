c28addedbf7b875d8eb79e859a3c333cf43829ea
Frank872321 <hknmmusic123@gmail.com>
2024-09-03 07:46:46 +0700

    my app first commit

--- forabc/settings.py
+++ forabc/settings.py
@@ -125,13 +125,6 @@ USE_TZ = True
 STATIC_URL = "main/static/"
 STATIC_ROOT = BASE_DIR / 'productionfiles'
 # This production code might break development mode, so we check whether we're in DEBUG mode
-if not DEBUG:
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
