e5ecfa1c8f09f0c740e400c6a0318485adf512fd
Frank872321 <hknmmusic123@gmail.com>
2024-09-03 08:22:20 +0700

    my app first commit

--- main/views.py
+++ main/views.py
@@ -6,5 +6,5 @@ def Main(request):
     queryset = Post.objects.all()
     some_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
     template = loader.get_template("index.html")
-    return HttpResponse(template.render({'queryset': queryset}, {'some_list': some_list}))
+    return HttpResponse(template.render({'queryset': queryset}))
  
